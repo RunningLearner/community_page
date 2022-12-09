fed7609e9175e6880db500fce6cec99053eed4fd
RunningLearner <tkacnsdms@naver.com>
2022-12-09 13:53:47 +0900

    feat:subs기능 추가

--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -1,24 +1,50 @@
 import { NextFunction, Request, Response, Router } from "express";
-import jwt from "jsonwebtoken";
 import User from "../entities/User";
 import userMiddleware from "../middlewares/user";
 import authMiddleware from "../middlewares/auth";
+import { isEmpty } from "class-validator";
+import { AppDataSource } from "../data-source";
+import Sub from "../entities/Sub";
 
 const createSub = async (req: Request, res: Response, next: NextFunction) => {
   const { name, title, description } = req.body;
 
   try {
-    let errors = {};
+    let errors: any = {};
 
-    const token = req.cookies.token;
-    if (!token) return next();
+    if (isEmpty(name)) errors.name = "이름을 비워둘 수 없습니다.";
+    if (isEmpty(name)) errors.title = "제목을 비워둘 수 없습니다.";
 
-    const { username }: any = jwt.verify(token, process.env.JWT_SECRET);
+    const sub = await AppDataSource.getRepository(Sub)
+      .createQueryBuilder("sub")
+      .where("lower(sub.name) = :name", { name: name.toLowerCase() })
+      .getOne();
 
-    const user = await User.findOneBy({ username });
+    if (sub) errors.name = "서브가 이미 존재합니다.";
 
-    if (!user) throw new Error("Unauthenticated");
-  } catch (error) {}
+    if (Object.keys(errors).length > 0) {
+      throw errors;
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
+
+  try {
+    const user: User = res.locals.user;
+
+    const sub = new Sub();
+    sub.name = name;
+    sub.title = title;
+    sub.description = description;
+    sub.user = user;
+
+    sub.save();
+    return res.json(sub);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "문제가 발생했습니다." });
+  }
 };
 
 const router = Router();
