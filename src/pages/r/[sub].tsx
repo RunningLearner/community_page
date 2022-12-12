cd50188b89e4eada3c97009ee52e71a9c67dc8e2
RunningLearner <tkacnsdms@naver.com>
2022-12-12 09:43:37 +0900

    feat: 누락된 import 추가

--- src/pages/r/[sub].tsx
+++ src/pages/r/[sub].tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { ChangeEvent, useEffect, useRef } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 import { useAuthState } from "../../context/auth";
 
