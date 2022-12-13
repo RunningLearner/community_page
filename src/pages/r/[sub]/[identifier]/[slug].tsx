e733ddb537f51eaf70832f1653761cca7f761c90
RunningLearner <tkacnsdms@naver.com>
2022-12-13 11:17:40 +0900

    feat: 좋아요 싫어요 UI 생성

--- src/pages/r/[sub]/[identifier]/[slug].tsx
+++ src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import classNames from "classnames";
 import dayjs from "dayjs";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -43,6 +44,32 @@ const PostPage = () => {
           {post && (
             <>
               <div className=" flex">
+                {/** 좋아요 싫어요 기능 부분 */}
+                <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                  {/** 좋아요 부분 */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    // onClick={() => vote(1)}
+                  >
+                    <i
+                      className={classNames("fas fa-arrow-up", {
+                        "text-red-500": post.userVote === 1,
+                      })}
+                    ></i>
+                  </div>
+                  <p className="text-xs font-bold">{post.voteScore}</p>
+                  {/** 싫어요 부분 */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                    // onClick={() => vote(-1)}
+                  >
+                    <i
+                      className={classNames("fas fa-arrow-down", {
+                        "text-blue-500": post.userVote === -1,
+                      })}
+                    ></i>
+                  </div>
+                </div>
                 <div className="py-2 pr-2">
                   <div className="flex items-center">
                     <p className=" text-xs text-gray-400">
@@ -118,6 +145,33 @@ const PostPage = () => {
               {/** 댓글 리스트 부분 */}
               {comments?.map((comment) => (
                 <div className="flex" key={comment.identifier}>
+                  {/** 좋아요 싫어요 기능 부분 */}
+
+                  <div className="flex-shrink-0 w-10 py-2 text-center rounded-l">
+                    {/** 좋아요 부분 */}
+                    <div
+                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                      // onClick={() => vote(1, comment)}
+                    >
+                      <i
+                        className={classNames("fas fa-arrow-up", {
+                          "text-red-500": comment.userVote === 1,
+                        })}
+                      ></i>
+                    </div>
+                    <p className="text-xs font-bold">{comment.voteScore}</p>
+                    {/** 싫어요 부분 */}
+                    <div
+                      className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
+                      // onClick={() => vote(-1, comment)}
+                    >
+                      <i
+                        className={classNames("fas fa-arrow-down", {
+                          "text-blue-500": comment.userVote === -1,
+                        })}
+                      ></i>
+                    </div>
+                  </div>
                   <div className="py-2 pr-2">
                     <p className="mb-1 text-xs leading-none">
                       <Link href={`/u/${comment.username}`} legacyBehavior>
