7d61d224fa8edcaf246452a6783a68e09d348515
RunningLearner <tkacnsdms@naver.com>
2022-12-15 11:54:01 +0900

    feat: 이미지허용 도메인 추가

--- client-repo/next.config.js
+++ client-repo/next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.gravatar.com", "localhost"],
+    domains: [
+      "www.gravatar.com",
+      "localhost",
+      "ec2-43-201-55-53.ap-northeast-2.compute.amazonaws.com",
+    ],
   },
 };
 
